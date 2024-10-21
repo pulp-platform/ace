60a1c9a1a0a7f25a7efa2cad10a7d886f021998e
Aleksi Korsman aleksi.korsman@aalto.fi <aleksi.korsman@aalto.fi>
2024-10-21 15:32:17 +0200

    Restructure and add combined aw/ar class

--- src/test/ace/ace_beat_types.svh
+++ src/test/ace/ace_beat_types.svh
@@ -3,23 +3,25 @@
 `endif
 /// The data transferred on a beat on the AW/AR channels.
 class ace_ax_beat #(
-    parameter AW = 32,
-    parameter IW = 8 ,
-    parameter UW = 1
+    parameter AW   = 32,
+    parameter IW   = 8 ,
+    parameter UW   = 1,
+    parameter SNP_W = 4
 );
-    rand logic [IW-1:0] id       = '0;
-    rand logic [AW-1:0] addr     = '0;
-    logic      [7:0]    len      = '0;
-    logic      [2:0]    size     = '0;
-    logic      [1:0]    burst    = '0;
-    logic               lock     = '0;
-    logic      [3:0]    cache    = '0;
-    logic      [2:0]    prot     = '0;
-    rand logic [3:0]    qos      = '0;
-    logic      [3:0]    region   = '0;
-    rand logic [UW-1:0] user     = '0;
-    rand logic [1:0]    bar      = '0;
-    rand logic [1:0]    domain   = '0;
+    rand logic [IW-1:0]    id       = '0;
+    rand logic [AW-1:0]    addr     = '0;
+    logic      [7:0]       len      = '0;
+    logic      [2:0]       size     = '0;
+    logic      [1:0]       burst    = '0;
+    logic                  lock     = '0;
+    logic      [3:0]       cache    = '0;
+    logic      [2:0]       prot     = '0;
+    rand logic [3:0]       qos      = '0;
+    logic      [3:0]       region   = '0;
+    rand logic [UW-1:0]    user     = '0;
+    rand logic [1:0]       bar      = '0;
+    rand logic [1:0]       domain   = '0;
+    rand logic [SNP_W-1:0] snoop    = '0;
 endclass
 
 class ace_aw_beat #(
@@ -27,10 +29,9 @@ class ace_aw_beat #(
     parameter IW = 8 ,
     parameter UW = 1
 ) extends ace_ax_beat #(
-  AW, IW, UW
+  .AW(AW), .IW(IW), .UW(UW), .SNP_W(3)
 );
     logic      [5:0] atop     = '0;
-    rand logic [2:0] snoop    = '0;
     rand logic       awunique = '0;
 endclass
 
@@ -39,9 +40,19 @@ class ace_ar_beat #(
     parameter IW = 8 ,
     parameter UW = 1
 ) extends ace_ax_beat #(
-  AW, IW, UW
+  .AW(AW), .IW(IW), .UW(UW), .SNP_W(4)
 );
-    rand logic [3:0] snoop = '0;
+endclass
+
+class ace_ax_comb_beat #(
+    parameter AW = 32,
+    parameter IW = 8 ,
+    parameter UW = 1
+) extends ace_ax_beat #(
+  .AW(AW), .IW(IW), .UW(UW), .SNP_W(4)
+);
+    logic      [5:0] atop     = '0;
+    rand logic       awunique = '0;
 endclass
 
 class ace_r_beat #(
