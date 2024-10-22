b503f4a67b9b0f805a1d2d7451411e78d4f22b56
Aleksi Korsman aleksi.korsman@aalto.fi <aleksi.korsman@aalto.fi>
2024-10-22 18:33:17 +0200

    Work toward cache model

--- src/test/cache/cache_scoreboard.svh
+++ src/test/cache/cache_scoreboard.svh
@@ -2,15 +2,35 @@
 *** INCLUDED IN cache_test_pkg ***
 `endif
 class cache_scoreboard #(
-    parameter int AW = 32
+    parameter int AW = 32,
+    parameter int DW = 32,
+    parameter int TW = 8,
+    parameter int WORD_WIDTH,
+    parameter int CACHELINE_WORDS,
+    parameter int WAYS,
 );
 
-    typedef logic [AW-1:0] addr_t;
-    typedef logic [7:0]    byte_t;
-    typedef logic [2:0]    status_t;
+    localparam int CACHELINE_BITS = WORD_WIDTH * CACHELINE_WORDS;
 
-    byte_t   memory_q[addr_t]; // Cache data
-    status_t status_q[addr_t]; // Cache state
+    typedef logic [TW-1:0]             tag_t;
+    typedef logic [AW-1:0]             addr_t;
+    typedef logic [7:0]                byte_t;
+    typedef logic [CACHELINE_BITS-1:0] cacheline_t;
+    typedef logic [2:0]                status_t;
+    
+    cacheline_t  data_q[addr_t];   // Cache data
+    status_t     status_q[addr_t]; // Cache state
+
+    mailbox #(cache_req)  cache_req_mbx;
+    mailbox #(cache_resp) cache_resp_mbx;
+
+    function new(
+        mailbox #(cache_req)  cache_req_mbx,
+        mailbox #(cache_resp) cache_resp_mbx
+    );
+        this.cache_req_mbx  = cache_req_mbx;
+        this.cache_resp_mbx = cache_resp_mbx;
+    endfunction
 
     function void init_mem_from_file(string fname);
         int fd, scanret;
@@ -31,5 +51,11 @@ class cache_scoreboard #(
         end
         $fclose(fd);
     endfunction
+
+    task cache_req;
+        cache_req req;
+        cache_resp resp;
+        cache_req_mbx.get(req);
+    endtask
 
 endclass
