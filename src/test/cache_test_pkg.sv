fbc093ec457b0adedbbd465e7f0f9cc3adc0f421
Aleksi Korsman aleksi.korsman@aalto.fi <aleksi.korsman@aalto.fi>
2024-10-22 18:34:33 +0200

    Add new files

--- src/test/cache_test_pkg.sv
+++ src/test/cache_test_pkg.sv
@@ -1,5 +1,7 @@
 package cache_test_pkg;
 `define _CACHE_TEST_PKG
+`include "cache/cache_beat_types.svh"
+`include "cache/cache_sequencer.svh"
 `include "cache/cache_scoreboard.svh"
 `include "cache/cache_top_agent.svh"
 endpackage
