e85923c1fcfdd5b71ce133444a582497df924f7c
Aleksi Korsman aleksi.korsman@aalto.fi <aleksi.korsman@aalto.fi>
2024-10-24 17:04:34 +0200

    Add types for cache->mem requests

--- src/test/cache/cache_beat_types.svh
+++ src/test/cache/cache_beat_types.svh
@@ -2,19 +2,44 @@
 *** INCLUDED IN cache_test_pkg ***
 `endif
 
+
+// Cache Requester operations
+localparam int REQ_LOAD        = 5'b00000;
+localparam int REQ_STORE       = 5'b00001;
+localparam int CMO_FLUSH_NLINE = 5'b10100;
+
+// Cache Memory operations
+localparam int MEM_READ   = 2'b00;
+localparam int MEM_WRITE  = 2'b01;
+localparam int MEM_ATOMIC = 2'b10;
+
+localparam int WR_POLICY_WB   = 3'b010;
+localparam int WR_POLICY_WT   = 3'b100;
+
 /// Datatype to orchestrate cache read and write requests
 class cache_req;
     int unsigned addr;
-    int unsigned data_q[$];
-    int unsigned len;
+    int unsigned data;
     int unsigned size;
-    bit          read;
-    bit          write;
+    int unsigned op;
+    bit          uncacheable;
+    bit          wr_policy_hint;
 endclass
 
 /// Datatype to orchestrate cache lookups between
 /// cache sequencer and cache scoreboard
 class cache_resp;
+    int unsigned data;
+endclass
+
+class mem_req;
+    int unsigned addr;
+    int unsigned len;
+    int unsigned size;
+    int unsigned op;
     int unsigned data_q[$];
-    bit          hit;
+endclass
+
+class mem_resp;
+    int unsigned data_q[$];
 endclass
