417da2d902b7477289e9dc388d38037328e1648f
Aleksi Korsman aleksi.korsman@aalto.fi <aleksi.korsman@aalto.fi>
2024-11-28 11:59:35 +0100

    Rename

--- src/ccu/ccu_mem_ctrl.sv
+++ src/ccu/ccu_mem_ctrl.sv
@@ -1,28 +1,31 @@
+`include "axi/assign.svh"
 module ccu_mem_ctrl import ace_pkg::*; #(
-    parameter int unsigned AxiIdWidth = 0,
-    parameter type req_t = logic,
-    parameter type resp_t = logic,
+    parameter type slv_req_t = logic,
+    parameter type slv_resp_t = logic,
+    parameter type mst_req_t = logic,
+    parameter type mst_resp_t = logic,
     parameter type aw_chan_t = logic,
     parameter type w_chan_t = logic
 )(
     input clk_i,
     input rst_ni,
-    input req_t wr_mst_req_i,
-    output resp_t wr_mst_resp_o,
-    input req_t r_mst_req_i,
-    output resp_t r_mst_resp_o,
-    output req_t mst_req_o,
-    input resp_t mst_resp_i
+    input slv_req_t wr_mst_req_i,
+    output slv_resp_t wr_mst_resp_o,
+    input slv_req_t r_mst_req_i,
+    output slv_resp_t r_mst_resp_o,
+    output mst_req_t mst_req_o,
+    input mst_resp_t mst_resp_i
 );
 
 localparam int unsigned FIFO_DEPTH = 4;
+parameter int unsigned SlvAxiIdWidth = $bits(r_mst_req_i.aw.id); // ID width in slv_req_t
 
 logic w_select, w_fifo_push, w_fifo_pop, w_fifo_empty, w_fifo_full;
 logic w_select_fifo;
 logic [$clog2(FIFO_DEPTH)-1:0] w_fifo_usage;
 logic w_valid, w_ready;
 logic aw_lock_d, aw_lock_q;
-req_t mst_req;
+slv_req_t mst_req;
 
 always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
@@ -51,24 +54,27 @@ stream_arbiter #(
 );
 
 // AR Channel (W-FSM cannot generate AR requests)
-assign mst_req.ar            = r_mst_req_i.ar;
-assign mst_req.ar_valid      = r_mst_req_i.ar_valid;
-assign r_mst_resp_o.ar_ready = mst_resp_i.ar_ready;
+assign mst_req.ar             = r_mst_req_i.ar;
+assign mst_req.ar_valid       = r_mst_req_i.ar_valid;
+assign r_mst_resp_o.ar_ready  = mst_resp_i.ar_ready;
+assign wr_mst_resp_o.ar_ready = 1'b0;
 
 // ID Prepending
 // Ensure that restrictive accesses get the same ID
 always_comb begin
-    mst_req_o.aw = mst_req.aw;
-    mst_req_o.ar = mst_req.ar;
+    mst_req_o.aw_valid = mst_req.aw_valid;
+    mst_req_o.ar_valid = mst_req.ar_valid;
+    `AXI_SET_AW_STRUCT(mst_req_o.aw, mst_req.aw)
+    `AXI_SET_AR_STRUCT(mst_req_o.ar, mst_req.ar)
     if (mst_req.aw.lock) begin
-        mst_req_o.aw.id = {2'b00, mst_req.aw.id[AxiIdWidth-1:0]};
+        mst_req_o.aw.id = {2'b00, mst_req.aw.id[SlvAxiIdWidth-1:0]};
     end else begin
-        mst_req_o.aw.id = {!w_select, w_select, mst_req.aw.id[AxiIdWidth-1:0]};
+        mst_req_o.aw.id = {!w_select, w_select, mst_req.aw.id[SlvAxiIdWidth-1:0]};
     end
     if (mst_req.ar.lock) begin
-        mst_req_o.ar.id = {2'b00, mst_req.ar.id[AxiIdWidth-1:0]};
+        mst_req_o.ar.id = {2'b00, mst_req.ar.id[SlvAxiIdWidth-1:0]};
     end else begin
-        mst_req_o.ar.id = {2'b01, mst_req.ar.id[AxiIdWidth-1:0]};
+        mst_req_o.ar.id = {2'b01, mst_req.ar.id[SlvAxiIdWidth-1:0]};
     end
 end
 
@@ -81,7 +87,7 @@ stream_mux #(
     .inp_valid_i({r_mst_req_i.w_valid, wr_mst_req_i.w_valid}),
     .inp_ready_o({r_mst_resp_o.w_ready, wr_mst_resp_o.w_ready}),
     .inp_sel_i  (w_select_fifo),
-    .oup_data_o (mst_req_o.w),
+    .oup_data_o (mst_req.w),
     .oup_valid_o(w_valid),
     .oup_ready_i(w_ready)
 );
@@ -113,6 +119,7 @@ assign aw_lock_d   = ~mst_resp_i.aw_ready && (mst_req_o.aw_valid || aw_lock_q);
 assign w_fifo_empty = w_fifo_usage == 0 && !w_fifo_full;
 
 // Block handshake if fifo empty
+`AXI_ASSIGN_W_STRUCT(mst_req_o.w, mst_req.w)
 assign mst_req_o.w_valid = w_valid            && !w_fifo_empty;
 assign w_ready           = mst_resp_i.w_ready && !w_fifo_empty;
 
@@ -120,7 +127,9 @@ assign w_ready           = mst_resp_i.w_ready && !w_fifo_empty;
 assign r_mst_resp_o.b  = mst_resp_i.b;
 assign wr_mst_resp_o.b = mst_resp_i.b;
 always_comb begin
-    if (mst_resp_i.b.id[AxiIdWidth+1:AxiIdWidth] == 2'b01) begin
+    r_mst_resp_o.b_valid  = 1'b0;
+    wr_mst_resp_o.b_valid = 1'b0;
+    if (mst_resp_i.b.id[SlvAxiIdWidth+1:SlvAxiIdWidth] == 2'b01) begin
         r_mst_resp_o.b_valid = mst_resp_i.b_valid;
         mst_req_o.b_ready    = r_mst_req_i.b_ready;
     end
@@ -134,7 +143,9 @@ end
 assign r_mst_resp_o.r  = mst_resp_i.r;
 assign wr_mst_resp_o.r = mst_resp_i.r;
 always_comb begin
-    if (mst_resp_i.r.id[AxiIdWidth+1:AxiIdWidth] == 2'b10) begin
+    wr_mst_resp_o.r_valid = 1'b0;
+    r_mst_resp_o.r_valid  = 1'b0;
+    if (mst_resp_i.r.id[SlvAxiIdWidth+1:SlvAxiIdWidth] == 2'b10) begin
         wr_mst_resp_o.r_valid = mst_resp_i.r_valid;
         mst_req_o.r_ready     = wr_mst_req_i.r_ready;
     end
@@ -143,5 +154,16 @@ always_comb begin
         mst_req_o.r_ready    = r_mst_req_i.r_ready;
     end
 end
+
+// pragma translate_off
+`ifndef VERILATOR
+initial begin : p_assert
+    assert(($bits(mst_req_o.aw.id) - $bits(r_mst_req_i.aw.id)) == 2)
+        else $fatal(1, "Difference in AW ID widths should be 2");
+    assert(($bits(mst_req_o.ar.id) - $bits(r_mst_req_i.ar.id)) == 2)
+        else $fatal(1, "Difference in AR ID widths should be 2");
+end
+`endif
+// pragma translate_on
 
 endmodule
