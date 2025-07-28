9ce8552cc2e621d254c41889bdd973e004735b3f
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-07-28 10:03:04 +0200

    license: fix headers

--- src/ace_snoop_cut.sv
+++ src/ace_snoop_cut.sv
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018 ETH Zurich, University of Bologna
+// Copyright (c) 2014-2025 ETH Zurich, University of Bologna
 //
 // Copyright and related rights are licensed under the Solderpad Hardware
 // License, Version 0.51 (the "License"); you may not use this file except in
