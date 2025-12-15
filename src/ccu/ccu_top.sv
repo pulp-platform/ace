fef6d6e6260dd05d3ad4156f10a424dedb8a7c46
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-12-15 15:57:20 +0100

    ccu: add authorship comments

--- src/ccu/ccu_top.sv
+++ src/ccu/ccu_top.sv
@@ -8,6 +8,9 @@
 // this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
+//
+// Authors:
+// - Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
 
 `include "axi/typedef.svh"
 `include "ace/typedef.svh"
